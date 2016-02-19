42af9d6ed9dbac4e8de4e57ce0e268f839c42072
Chris Slowik <chris.slowik@gmail.com>
2016-02-19 13:48:49 -0800

    added lesson 2

---
+++ lessons/lesson-two/modlr/modlr/Extensions/MODLR-UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  MODLR-UIColor.swift
+//  MODLR-TEST
+//
+//  Created by Chris Slowik on 1/12/16.
+//  Copyright © 2016 Creativedash. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // base yellow for all the things
+    class func lightYellow() -> UIColor {
+        return UIColor(red:1,  green:0.965,  blue:0.353, alpha:1)
+    }
+    
+    // secondary, slightly darker yellow shade
+    class func medYellow() -> UIColor {
+        return UIColor(red:1,  green:0.910,  blue:0.353, alpha:1)
+    }
+}
---
+++ lessons/lesson-two/modlr/modlr/Extensions/MODLR-UIView.swift
@@ -0,0 +1,32 @@
+//
+//  MODLR-UIView.swift
+//  MODLR-TEST
+//
+//  Created by Chris Slowik on 1/11/16.
+//  Copyright © 2016 Creativedash. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func createYellowGradientBG(color1: UIColor, color2: UIColor) {
+        // make sure the view has accounted for constraints etc
+        layoutIfNeeded()
+        
+        // create the gradient layer and set it to the same size as the view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+        
+        // set up the gradient colors
+        gradientLayer.colors = [color1.CGColor, color2.CGColor]
+        
+        // gradient locations and start/end points
+        gradientLayer.locations = [0, 1]
+        gradientLayer.startPoint = CGPoint(x: 0.35, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.65, y: 1)
+        
+        self.layer.insertSublayer(gradientLayer, atIndex: 0)
+    }
+    
+}
